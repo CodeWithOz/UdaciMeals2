f73be01822586aa37b3b2fec878cfb0c55634815
Uche Ozoemena <ucheozoemena@gmail.com>
2018-10-29 18:15:12 +0100

    Populate the order manager

--- app/services/order-manager.js
+++ app/services/order-manager.js
@@ -1,4 +1,26 @@
 import Service from '@ember/service';
 
-export default Service.extend({
-});
+const OrderManagerOptions = {
+  selectedDay: 'Monday',
+  menuSelection: {
+    Monday: {},
+    Tuesday: {},
+    Wednesday: {},
+    Thursday: {},
+    Friday: {}
+  },
+
+  setSelectedDayTo(day) {
+    this.set('selectedDay', day);
+  },
+
+  chooseMenuOption(mealCategory, menuItemName) {
+    this.set(`menuSelection.${this.get('selectedDay')}.${mealCategory}`, menuItemName);
+  },
+
+  removeMenuOption(day, mealCategory) {
+    this.set(`menuSelection.${day}.${mealCategory}`, '');
+  }
+};
+
+export default Service.extend(OrderManagerOptions);
