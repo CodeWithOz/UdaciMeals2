383b739cdd768618ef5b4f7181df5881cd88d9b0
Uche Ozoemena <ucheozoemena@gmail.com>
2018-10-30 06:54:34 +0100

    Handle updates to selected meal

--- app/components/menu-item.js
+++ app/components/menu-item.js
@@ -5,9 +5,14 @@ export default Component.extend({
   orderManager: inject('order-manager'),
   tagNamess: 'li',
   classNames: ['menu-item'],
+  mealCategory: 'breakfast',
   actions: {
     chooseItem(menuItemName) {
-      this.get('orderManager').chooseMenuOption('breakfast', menuItemName);
+      this.get('orderManager').chooseMenuOption(this.get('mealCategory'), menuItemName);
+    },
+
+    changeCategory(category) {
+      this.set('mealCategory', category);
     }
   }
 });
