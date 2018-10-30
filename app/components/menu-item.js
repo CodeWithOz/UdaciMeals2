e47765c2ddf60e5edc2b0ba5c1b714621a83bb5c
Uche Ozoemena <ucheozoemena@gmail.com>
2018-10-30 06:45:32 +0100

    Update order manager on meal selection
    This only updates the selected food but not the meal. It's still
    hard-coded to only update breakfast.

--- app/components/menu-item.js
+++ app/components/menu-item.js
@@ -1,6 +1,13 @@
 import Component from '@ember/component';
+import { inject } from '@ember/service';
 
 export default Component.extend({
+  orderManager: inject('order-manager'),
   tagNamess: 'li',
-  classNames: ['menu-item']
+  classNames: ['menu-item'],
+  actions: {
+    chooseItem(menuItemName) {
+      this.get('orderManager').chooseMenuOption('breakfast', menuItemName);
+    }
+  }
 });
